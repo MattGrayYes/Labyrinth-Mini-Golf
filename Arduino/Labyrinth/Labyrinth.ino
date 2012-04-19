5ad9480df3c6448a03815ca72bc3bbb6da0cda71
Matt Gray <git@mattg.co.uk>
2012-04-19 20:36:55 +0100

    Replaced Labyrinth.ino with the finished file. I accidentally uploaded an initial test the first time

--- Arduino/Labyrinth/Labyrinth.ino
+++ Arduino/Labyrinth/Labyrinth.ino
@@ -1,6 +1,6 @@
-// Sweep
-// by BARRAGAN <http://barraganstudio.com> 
-// This example code is in the public domain.
+// Labyrinth Mini Golf
+// Arduino Controller by Matt Gray
+// http://www.mattg.co.uk/projects/golf
 
 
 #include <Servo.h> 
@@ -11,19 +11,17 @@
 #include "WiiChuck.h"
 #include "nunchuck_funcs.h"
 
-Servo myservo;  // create servo object to control a servo 
-                // a maximum of eight servo objects can be created 
- 
-int pos = 0;    // variable to store the servo position 
- 
+Servo Xservo, Yservo;  // objects to control the two servos
+
 WiiChuck chuck = WiiChuck(); 
 
 void setup() 
 { 
-  myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
-  nunchuck_setpowerpins();
+  Xservo.attach(9);  // stick one servo on pin 9
+  Yservo.attach(10); // and another on pin 10
+  nunchuck_setpowerpins();	// lets the Wiichuck adapter work on the analog pins
   
-  Serial.begin(115200);
+  Serial.begin(115200);		// start serial for debug info
   
   chuck.begin();
   chuck.update();
@@ -34,44 +32,70 @@ void setup()
 void loop() 
 { 
   chuck.update(); 
-  
-  int valtot =0;
-  int val=0;
+  int Xpos=0;
+  int Ypos=0; 
+  int Xvaltot =0;
+  int Yvaltot =0;
+  int Xval=0;
+  int Yval =0;
   int i;
   int n = 25;
-  int readings[n];
+  int Xreadings[n];
+  int Yreadings[n];
   
-  //gather readings over period of time
+  //gather n readings
   for(i=0; i<n; i++)
   {
     chuck.update(); 
-    readings[i] = chuck.readJoyX();
+    Xreadings[i] = chuck.readJoyX();
+    Yreadings[i] = chuck.readJoyY();
       //delay(20); 
   }
   
   //create average
   for(i=0; i<n; i++)
   {
-    valtot = valtot+readings[i];
+    Xvaltot = Xvaltot+Xreadings[i];
+    Yvaltot = Yvaltot+Yreadings[i];
   }
-  val = valtot/i;
+  Xval = Xvaltot/i;
+  Yval = Yvaltot/i;
   
   // map joystick value to degrees  
-  pos = map(val, -130, 130, 70, 110);
-  if( ((val > 0) && (val <= 2)) || ((val >= -2) && (val < 0)) )
+  Xpos = map(Xval, -130, 130, 70, 110);
+  //correct for noise around 0 to help keep flat
+  if( ((Xval > 0) && (Xval <= 2)) || ((Xval >= -2) && (Xval < 0)) )
   {
-    pos = 90;
-    Serial.print("Correction");
+    Xpos = 90;
+    Serial.print("X Correction");
+  }
+  else
+    Serial.print("          ");
+    
+  // map joystick value to degrees  
+  Ypos = map(Yval, -130, 130, 70, 110);
+  // correct for noise around 0 to help keep flat
+  if( ((Yval > 0) && (Yval <= 2)) || ((Yval >= -2) && (Yval < 0)) )
+  {
+    Ypos = 90;
+    Serial.print("Y Correction");
   }
   else
     Serial.print("          ");
   
-  myservo.write(pos);              // tell servo to go to position in variable 'pos' 
-  //delay(15);                       // waits 15ms for the servo to reach the position 
+  Xservo.write(Xpos);              // tell servo to go to position in variable 'pos' 
+  Yservo.write(Ypos);
+  //delay(15);                       // waits for the servo to reach the position 
   
-  Serial.print("   Joy: ");
-  Serial.print(val);
-  Serial.print(" \t\t Pos: ");
-  Serial.println(pos);
+  //print debug shizz.
+  Serial.print("   XJoy: ");
+  Serial.print(Xval);
+  Serial.print(" \t\t XPos: ");
+  Serial.println(Xpos);
+  
+  Serial.print(" \t\tYJoy: ");
+  Serial.print(Yval);
+  Serial.print(" \t\t YPos: ");
+  Serial.println(Ypos);
  
 } 
